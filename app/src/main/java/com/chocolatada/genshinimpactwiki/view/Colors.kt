3ee8975166da0f18255555572c97788fbbee3855
Ulises Justo Saucedo <ulisesjustosaucedo@gmail.com>
2024-12-27 13:03:06 -0300

    Resources for UI

---
+++ app/src/main/java/com/chocolatada/genshinimpactwiki/view/Colors.kt
@@ -0,0 +1,7 @@
+package com.chocolatada.genshinimpactwiki.view
+
+import androidx.compose.ui.graphics.Color
+
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val BlueDT = Color(0xFF181B1F)
---
+++ app/src/main/java/com/chocolatada/genshinimpactwiki/view/Font.kt
@@ -0,0 +1,7 @@
+package com.chocolatada.genshinimpactwiki.view
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.chocolatada.genshinimpactwiki.R
+
+val GenshinImpactFont = FontFamily(Font(R.font.genshinfont))
