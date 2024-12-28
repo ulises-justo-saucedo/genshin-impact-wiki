dc50627a2f0c7951255ea323096e912999204470
Ulises Justo Saucedo <ulisesjustosaucedo@gmail.com>
2024-12-28 11:21:35 -0300

    AppModule added for Ktor

---
+++ app/src/main/java/com/chocolatada/genshinimpactwiki/di/AppModule.kt
@@ -0,0 +1,27 @@
+package com.chocolatada.genshinimpactwiki.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideHttpClient(): HttpClient {
+        return HttpClient {
+            install(ContentNegotiation) {
+                json(Json {
+                    ignoreUnknownKeys = true
+                })
+            }
+        }
+    }
+}
