bcb749f4f6b79787b40cdadd66758b30a016dbad
Ulises Justo Saucedo <ulisesjustosaucedo@gmail.com>
2024-12-29 14:37:27 -0300

    DTOs for Views

---
+++ app/src/main/java/com/chocolatada/genshinimpactwiki/domain/dto/CharacterSearchScreenDTO.kt
@@ -0,0 +1,8 @@
+package com.chocolatada.genshinimpactwiki.domain.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CharacterSearchScreenDTO(
+    val id: String
+)
--- app/src/main/java/com/chocolatada/genshinimpactwiki/domain/dto/FieldMainScreenDTO.kt
+++ app/src/main/java/com/chocolatada/genshinimpactwiki/domain/dto/FieldMainScreenDTO.kt
@@ -2,5 +2,5 @@ package com.chocolatada.genshinimpactwiki.domain.dto
 
 data class FieldMainScreenDTO(
     val key: String,
-    val icon: Int,
+    val icon: Int
 )
